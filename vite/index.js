d9ed855d84db8e313a1b08778561f4997872bb28
wu kai <wukai2048@gmail.com>
2022-01-28 18:13:59 +0800

    移动文件位置

--- vite/index.js
+++ vite/index.js
@@ -1,9 +1,21 @@
 const fs = require('fs');
+const path = require('path');
 const http = require('http');
 const connect = require('connect');
+const { dirname } = require('path');
 const middlewares = connect();
 
+const cacheDir = path.join(__dirname, 'node_modules/.vite');
+
+const optimizeDeps = () => {
+  fs.mkdirSync(cacheDir, { recursive: true });
+
+  const deps = Object.keys(require('../package.json').dependencies);
+  console.log('deps: ', deps);
+};
+
 const createServer = async () => {
+  await optimizeDeps();
   http.createServer(middlewares).listen(3000, () => {
     console.log('Server running on port 3000');
   });
@@ -11,7 +23,7 @@ const createServer = async () => {
 
 const indexHtmlMiddleware = (req, res, next) => {
   if (req.url === '/') {
-    const htmlPath = './index.html';
+    const htmlPath = path.resolve(dirname, '../index.html');
     const htmlContent = fs.readFileSync(htmlPath, 'utf8');
     res.setHeader('Content-Type', 'text/html');
     res.statusCode = 200;
